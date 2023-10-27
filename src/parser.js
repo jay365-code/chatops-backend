5e134ad7fb410f999012d0cd34fd49db488ec49c
jay365 <jhlee@gubed.co.kr>
2023-10-27 11:49:30 +0900

    1차 완료번 2번째

--- src/parser.js
+++ src/parser.js
@@ -1,90 +1,173 @@
-class Output {
-    constructor(provider, numNodes, location) {
-        this.provider = provider;
-        this.numNodes = numNodes;
-        this.location = location;
+const cmpApi = require("./cmp-if.js");
+
+class Instance {
+  constructor(numCPU, sizeMemory, osTemplate, sizeStorage, numNode) {
+    this._numCPU = numCPU;
+    this._sizeMemory = sizeMemory;
+    this._osTemplate = osTemplate;
+    this._sizeStorage = sizeStorage;
+    this._numNode = numNode;
+  }
+
+  // Getter 메서드
+  get numCPU() {
+    return this._numCPU;
+  }
+
+  get sizeMemory() {
+    return this._sizeMemory;
+  }
+
+  get osTemplate() {
+    return this._osTemplate;
+  }
+
+  get sizeStorage() {
+    return this._sizeStorage;
+  }
+  get numNode() {
+    return this._numNode;
+  }
+
+  // Setter 메서드
+  set numCPU(value) {
+    this._numCPU = value;
+  }
+
+  set sizeMemory(value) {
+    this._sizeMemory = value;
+  }
+
+  set osTemplate(value) {
+    this._osTemplate = value;
+  }
+
+  set sizeStorage(value) {
+    this._sizeStorage = value;
+  }
+  set numNode(value) {
+    this._numNode = value;
+  }
+}
+
+function makeInstanceContext(req) {
+  const context = `Context:\n-----------\n코어 수량: ${
+    req.session.instance.numCPU || "미입력"
+  }\n메모리 크기: ${req.session.instance.sizeMemory || "미입력"}\nOS 템플릿: ${
+    req.session.instance.osTemplate || "미입력"
+  }\n스토리지 크기: ${
+    req.session.instance.sizeStorage || "미입력"
+  }\n노드 수량: ${req.session.instance.numNode || "미입력"}\n-----------\n`;
+
+  // console.log("context=" + context);
+
+  return context;
+}
+
+function makeClusterContext(req) {
+  const context = `Context:\n-----------\n코어 수량: ${
+    req.session.cluster.numCPU || "미입력"
+  }\n메모리 크기: ${req.session.cluster.sizeMemory || "미입력"}\nOS 템플릿: ${
+    req.session.cluster.osTemplate || "미입력"
+  }\n스토리지 크기: ${
+    req.session.cluster.sizeStorage || "미입력"
+  }\s노드 수량: ${req.session.cluster.numNode || "미입력"}\n-----------\n`;
+
+  // console.log("context=" + context);
+
+  return context;
+}
+
+function parseText(req, text) {
+  let numCPU = null;
+  let sizeMemory = null;
+  let osTemplate = null;
+  let sizeStorage = null;
+  let numNode = null;
+
+  // Find the index where JSON object starts
+  const startIndex = text.indexOf("###");
+
+  // If '###' is not found, assign empty strings
+  const regularText =
+    startIndex !== -1 ? text.slice(0, startIndex).trim() : text;
+  const jsonText = startIndex !== -1 ? text.slice(startIndex + 3) : ""; // If '###' not found, jsonText is an empty string
+
+  // console.log(
+  //   "jsonText: Text=" +
+  //     jsonText +
+  //     ", length=" +
+  //     String(jsonText).length +
+  //     ", index=" +
+  //     startIndex
+  // );
+
+  if (!jsonText) {
+    // JSON 형식의 text가 없을 경우, output을 null로 리턴
+    return { regularText, output: null };
+  }
+
+  try {
+    // Parse JSON text to JavaScript object
+    const correctedJsonText = jsonText.replace(/\\/g, "");
+    console.log("collectedJsonText=" + correctedJsonText);
+    const jsonObject = JSON.parse(correctedJsonText);
+
+    if (jsonObject["Output"]) {
+      // Extract the properties of the Output class from jsonObject
+      numCPU = parseInt(jsonObject["Output"].numCPU) || null;
+      if (numCPU) {
+        sizeMemory = parseInt(jsonObject["Output"].sizeMemory) || null;
+        osTemplate = jsonObject["Output"].osTemplate || null;
+      }
+      sizeStorage = parseInt(jsonObject["Output"].sizeStorage) || null;
+      numNode = parseInt(jsonObject["Output"].numNode) || null;
+      const workIntention = jsonObject["Output"].workIntention || null;
+      const intent = jsonObject["Output"].intent || null;
+
+      // TBD: CPU 수량이 특정되지 않았을 때 메모리, OS 템플릿 입력 처리 방안
+      // 1. 코드에서 코어 수량 미입력 시 메모리, OS 템플릿 입력값을 처리하지 않도록 하는 방법이 있고
+      // 2. Prompt에서 입력 처리를 하지 않거나 Validation 처리하는 방법이 있는데. Prompt 처리할 경우 예외적인 상황이 벌어질 확률이 확률이 많을 것으로 판단됨.
+      // 우선 Prompt에서 처리하도록 해보고 안되면 코드에서 처리하는 것으로 해보자.
+      // 시험 결과: Prompt에서 값을 추출하는 것을 막기는 어려다. 대화상으로는 자연스럽지만 값을 강제하진 못함.
+
+      // Create an Output object
+      const output = new Instance(
+        numCPU,
+        sizeMemory,
+        osTemplate,
+        sizeStorage,
+        numNode
+      );
+
+      // numCPU가 선택되면 numCPU에 따른 memory 리스트를 CMP에서 조회
+      // console.log("------------>sizeMemory=" + sizeMemory);
+      if (numCPU) {
+        cmpApi.getRecommendedMemory(req, numCPU);
+        cmpApi.getRecommendedOS(req, numCPU);
+
+        // console.log(
+        //   "at parseText(), req.session.instanceMemoryList=" +
+        //     req.session.instanceMemoryList
+        // );
+      }
+      return { regularText, output, workIntention, intent };
+    } else {
+      console.error("JSON does not contain Output property");
+      return { regularText, output: null, workIntention: null, intent: null };
     }
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+    // JSON 파싱에 오류가 발생한 경우, output을 null로 리턴
+    return { regularText, output: null, workIntention: null, intent: null };
+  }
 }
 
-function parseText(text) {
-    // Find the index where JSON object starts
-    const startIndex = text.indexOf('###');
-    
-    // Extract the regular text and JSON text
-    const regularText = text.slice(0, startIndex).trim();
-    const jsonText = text.slice(startIndex + 3); // Skip the '###'
-
-    console.log("jsonText: Text=" + jsonText + ", length=" + String(jsonText).length + ", index=" + startIndex);
-    
-    if (!jsonText) {
-        // JSON 형식의 text가 없을 경우, output을 null로 리턴
-        return { regularText, output: null };
-    }
-    
-    try {
-        // Parse JSON text to JavaScript object
-        const jsonObject = JSON.parse(jsonText);
-        
-        if (jsonObject['Output']) {
-            // Extract the properties of the Output class from jsonObject
-            const provider = jsonObject['Output'].Provider || null;
-            const numNodes = jsonObject['Output'].NumNodes || null;
-            const location = jsonObject['Output'].Location || null;
-            
-            // Create an Output object
-            const output = new Output(provider, numNodes, location);
-            
-            return { regularText, output };
-        } else {
-            console.error("JSON does not contain Output property");
-            return { regularText, output: null };
-        }
-    } catch (error) {
-        console.error("Error parsing JSON:", error);
-        // JSON 파싱에 오류가 발생한 경우, output을 null로 리턴
-        return { regularText, output: null };
-    }
-}
-
-
-
-// const text = `지역 정보가 누락되었습니다. 클러스터를 생성하기 위해선 지역 정보가 필요합니다. 지역 정보를 입력해주세요.
-
-// {
-//     "###Output": {
-//         "Provider": "AWS",
-//         "NumNodes": "5",
-//         "Location": "미입력"
-//     }
-// }`;
-
-// const { regularText, output } = parseText(text);
-
-// console.log(regularText);
-// console.log(output.provider);
-
-const inputHistory = new Output("Azure");
-
-function makeContext(inputHistory) {
-    const provider = inputHistory.provider || "미입력";
-    const numNodes = inputHistory.numNodes || "미입력";
-    const location = inputHistory.location || "미입력";
-
-    const context = `Context:\n--------------------\n클라우드 사업자: ${provider}\n노드 수량: ${numNodes}\n지역: ${location}\n--------------------\n`;
-    
-    return context;
-}
-
-function resetContex() {
-
-}
-
-// const context = makeContext(inputHistory);
-// console.log(context);
-
+function resetContex() {}
 
 module.exports = {
-    Output: Output,
-    parseText: parseText,
-    makeContext: makeContext
-}
+  Instance: Instance,
+  parseText: parseText,
+  makeInstanceContext: makeInstanceContext,
+  makeClusterContext: makeClusterContext,
+};
--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const openai = require("openai");
 // });
 
 const chatParser = require("./parser.js");
+const chatFlow = require("./chatbot-flow.js");
 const session = require("express-session");
 // const secretKey = process.env.SESSION_SECRET || 'myverylongrandomsecretkey';
 const secretKey = process.env.SESSION_SECRET || "LSJLK-SLD83-DLFJG-28375-JHLEE";
@@ -21,7 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:8080", // 허용할 origin (프론트엔드 앱의 주소)
+    origin: ["http://localhost:8080", "http://172.16.10.168:3001/"], // 허용할 origin (프론트엔드 앱의 주소)
     credentials: true, // credentials 모드 사용
   })
 );
@@ -38,12 +39,6 @@ const API_URL =
   "https://www.stack-inference.com/run_deployed_flow?flow_id=64ab8f4d036adfef3faee13f&org=0b713554-21ff-409a-b843-985bfc350d9c";
 const headers = {
   Authorization: "Bearer public_key",
-  "Content-Type": "application/json",
-};
-
-const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
-const openaiHeaders = {
-  Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
   "Content-Type": "application/json",
 };
 
@@ -75,14 +70,16 @@ app.post("/query", (req, res) => {
       const { regularText, output } = chatParser.parseText(
         response.data["out-0"]
       );
-      // console.log("completion: " + regularText + ", provider: " + output.provider);
+      // console.log("aiResponse: " + regularText + ", provider: " + output.provider);
       if (output !== null) {
         req.session.output = JSON.parse(JSON.stringify(output)); // 세션에 입력 정보 저장
+        console.log(
+          "response session.output: " + JSON.stringify(req.session.output)
+        );
+        res.json({ output: regularText });
+      } else {
+        res.json({ output: process.env.EXCPTION_NULL_ANSWWER });
       }
-      console.log(
-        "response session.output: " + JSON.stringify(req.session.output)
-      );
-      res.json({ output: regularText });
     })
     .catch((error) => {
       console.error(error);
@@ -90,83 +87,18 @@ app.post("/query", (req, res) => {
     });
 });
 
-app.post("/chat", (req, res) => {
-  // 세션에서 입력 정보를 가져와 Context 완성
-  if (req.session.output === undefined) {
-    req.session.output = new chatParser.Output("", 0, "");
-    console.log("request session.output: 초기화");
-  } else {
-    console.log(
-      "request session.output: " + JSON.stringify(req.session.output)
-    );
+app.post("/chat", async (req, res) => {
+  try {
+    const aiResponse = await chatFlow.chattingFlow(req); // 'await'를 사용해 chattingFlow에서 반환되는 값을 기다립니다.
+    console.log("A: " + aiResponse);
+    res.json({ output: aiResponse });
+  } catch (error) {
+    console.error("Error getting AI response:", error);
+    res.status(500).json({ error: "Failed to get AI response" });
   }
-
-  const context = `Context:\n-----------\n클라우드 사업자: ${
-    req.session.output.provider || "미입력"
-  }\n노드 수량: ${req.session.output.numNodes || "미입력"}\n지역: ${
-    req.session.output.location || "미입력"
-  }\n-----------\n`;
-  //   req.body["in-0"] = context + JSON.stringify(req.body);
-  //   console.log("chat req.body: " + JSON.stringify(req.body));
-
-  //   const envSystemContent =
-  //     process.env.OPENAI_CHAT_SYSTEM_PROMPT +
-  //     process.env.OPENAI_CHAT_SYSTEM_PROMPT2;
-  //   console.log(envSystemContent);
-
-  const systemContent = `${process.env.OPENAI_CHAT_SYSTEM_PROMPT}\n###${process.env.OPENAI_CHAT_SYSTEM_PROMPT2}\nprompt:\n${context}`; // system + 변수로 사용할 출력 형식 지정, 분리한 이유는 string에 JSON 형식이 있으면 JSON 형식부터 잘리는 현상 방지를 위해 분리함
-  const userContent = req.body["in-0"];
-  console.log(systemContent);
-  console.log(userContent);
-  //   console.log(process.env.OPENAI_CHAT_ASSISTANT_PROMPT);
-
-  axios
-    .post(
-      OPENAI_API_URL,
-      {
-        model: "gpt-4",
-        messages: [
-          {
-            role: "system",
-            content: systemContent,
-          },
-          {
-            role: "user",
-            content: userContent,
-          },
-          //   ,
-          //   {
-          //     role: "assistant",
-          //     content: process.env.OPENAI_CHAT_ASSISTANT_PROMPT,
-          //   },
-        ],
-        temperature: 1,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      },
-      { headers: openaiHeaders }
-    )
-    .then((response) => {
-      const completion = response.data.choices[0].message.content;
-      console.log("completion: " + completion);
-      const { regularText, output } = chatParser.parseText(completion);
-      // console.log("completion: " + regularText + ", provider: " + output.provider);
-      if (output !== null) {
-        req.session.output = JSON.parse(JSON.stringify(output)); // 세션에 입력 정보 저장
-      }
-      console.log(
-        "response session.output: " + JSON.stringify(req.session.output)
-      );
-      res.json({ output: regularText });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send(error);
-    });
 });
 
 app.listen(5050, () => {
   console.log("Server is running on port 5050");
+  console.log(process.env.OPENAI_API_KEY);
 });
